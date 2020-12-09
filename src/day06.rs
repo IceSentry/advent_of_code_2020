31e20f76f266ec17a18fe32607f72440fd69c250
IceSentry <c.giguere42@gmail.com>
2020-12-08 20:50:36 -0500

    start day06

---
+++ src/day06.rs
@@ -0,0 +1,62 @@
+use itertools::Itertools;
+use serde_scan::scan;
+
+fn find_sum(input: &[i32], n: usize) -> Vec<i32> {
+    input
+        .iter()
+        .copied()
+        .combinations(n)
+        .find(|x| x.iter().sum::<i32>() == 2020)
+        .expect("There should be a valid combination")
+}
+
+pub fn parse(input: &str) -> Vec<i32> {
+    input.lines().map(|l| scan!("{}" <- l).unwrap()).collect()
+}
+
+pub fn part_1(input: &[i32]) -> i32 {
+    let sums = find_sum(input, 2 as usize);
+    sums.iter().product::<i32>()
+}
+
+pub fn part_2(input: &[i32]) -> i32 {
+    let sums = find_sum(input, 3 as usize);
+    sums.iter().product::<i32>()
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    const INPUTS: &str = indoc! {"
+        abc
+
+        a
+        b
+        c
+        
+        ab
+        ac
+        
+        a
+        a
+        a
+        a
+        
+        b
+    "};
+
+    // #[test]
+    // pub fn part_1() {
+    //     let input = super::parse(INPUTS);
+    //     let result = super::part_1(&input);
+    //     assert_eq!(result, 11);
+    // }
+
+    // #[test]
+    // pub fn part_2() {
+    //     let input = super::parse(INPUTS);
+    //     let result = super::part_2(&input);
+    //     assert_eq!(result, 241861950);
+    // }
+}
